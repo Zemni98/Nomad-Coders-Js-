29e37cabffbce354153af69a016fb353e4be3d7d
Zemni98 <103926986+Zemni98@users.noreply.github.com>
2022-06-29 21:50:07 +0900

    Delete js directory

--- js/background.js
+++
@@ -1,9 +0,0 @@
-const iamges = ["0.jpeg", "1.jpeg", "2.jpeg"];
-
-const chosenImage = iamges[Math.floor(Math.random() * iamges.length)];
-
-const bgImage = document.createElement("img");
-
-bgImage.src = `img/${chosenImage}`;
-
-document.body.appendChild(bgImage);
